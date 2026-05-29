baedaae5bdc6dcf0046f35cfcbba363a50cfa4cd
RAHUL <rahul.210092004@gmail.com>
2026-05-29 17:38:24 +0530

    fix: express path-to-regexp crash on wildcard route

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ app.get('/api/health', (req, res) => {
 // Serve frontend in production environment
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
