5983ccb4abbae228fa607effc43a1efbbc0d8b54
RAHUL <rahul.210092004@gmail.com>
2026-05-29 17:57:43 +0530

    fix: set auth cookies to SameSite=none for cross-origin hosting

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -42,14 +42,14 @@ exports.login = async (req, res) => {
     res.cookie('token', accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 15 * 60 * 1000 // 15 mins
     });
 
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
@@ -118,7 +118,7 @@ exports.refresh = async (req, res) => {
     res.cookie('token', newAccessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 15 * 60 * 1000 // 15 mins
     });
 
